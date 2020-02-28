ec91af16d675346fb17ec6f52aad458216a97558
Tordon <Tordon@DESKTOP-G4ANKN4>
2020-02-28 16:21:48 +0100

    alapok megcsinálása

--- EloadasProject/Program.cs
+++ EloadasProject/Program.cs
@@ -2,11 +2,46 @@
 
 namespace EloadasProject
 {
-    class Program
+    class Eloadas
     {
+        private bool[,] foglalasok;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+
+        public Eloadas(int sorokSzama,int helyekSzama)
+        {
+            if (sorokSzama > 0 && helyekSzama > 0)
+            {
+                foglalasok = new bool[sorokSzama,helyekSzama];
+            }
+            else
+            {
+                ArgumentException e;
+            }
+        }
+        public bool Lefoglal()
+        {
+            return false;
+        }
+        public int SzabadHelyek { get; }
+        public bool Teli()
+        {
+            return false;
+        }
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam > 0 && helySzam > 0)
+            {
+
+            }
+            else
+            {
+                ArgumentException e;
+            }
+            return false;
+        }
+
     }
 }
